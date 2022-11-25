ea3a58fcbc6e92cf9df644b68e12841007fa3731
+ <+>
2022-11-25 09:15:20 +0300

    Done with assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+const { returns } = require("chai-spies");
+
+  function receivesAFunction (callback) 
+ {
+     callback();
+ }
+ //names function 
+ function divide(a ,b){
+    return ( a/b)
+ }
+ function returnsANamedFunction(){
+        
+    return (divide)}
+
+     function returnsAnAnonymousFunction(){
+        return function(){
+            console.log("Anonymous!")
+        }
+    }
+     
+       
+
+    
+
+ 
+
+    
